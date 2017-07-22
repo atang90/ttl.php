9ad9af78eb73cfebe6c536ca9f3281a74cc521f9
atang90 <andrew.tang90@gmail.com>
2017-07-21 22:27:43 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <?php include "/Users/Andrew/Sites/Projects/angular/inc/header.php";?>
+    <title><?php print $_SERVER['SERVER_NAME'] . " - Homepage"?></title>
+
+  </head>
+
+  <body>
+
+    <?php include "/Users/Andrew/Sites/Projects/angular/inc/navbar.php";?>
+
+    <main>
+
+    </main>
+
+  </body>
+
+  <footer>
+
+    <?php include "/Users/Andrew/Sites/Projects/angular/inc/footer.php";?>
+
+  </footer>
+
+</html>
