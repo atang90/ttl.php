a67e1b0803e05b0184a15b6ec38683e8ea476faa
atang90 <andrew.tang90@gmail.com>
2017-07-21 22:27:06 -0400

    Create template.php

---
+++ template.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <?php include "/Users/Andrew/Sites/Projects/angular/inc/header.php";?>
+    <title><?php print $_SERVER['SERVER_NAME'] ?></title>
+
+  </head>
+
+  <body>
+
+    <?php include "/Users/Andrew/Sites/Projects/angular/inc/navbar.php";?>
+
+    <main>
+
+    </main>
+
+  </body>
+
+  <footer>
+
+    <?php include "/Users/Andrew/Sites/Projects/angular/inc/footer.php";?>
+
+  </footer>
+
+</html>
