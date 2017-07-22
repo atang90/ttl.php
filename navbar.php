6519cb1c06bf1920a98ae15f434aab5a65472386
atang90 <andrew.tang90@gmail.com>
2017-07-21 22:28:54 -0400

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!-- /// Navigation Bar /// -->
+<div class="navbar-fixed">
+  <nav class="light-blue darken-3">
+  <div class="nav-wrapper" style="margin-left:10px"><a href="index.html" class="brand-logo">Throughthelens.gallery</a>
+    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down valign-wrapper" id="menusymbols" style="margin-right:10px">
+      <li class="waves-effect waves-default"><a href="#"><img src="img/icons/gallery.svg"><span style="padding-left:7px">Gallery</span></a></li>
+      <li class="waves-effect waves-default"><a href="contact.html"><img src="img/icons/contact.svg"><span style="padding-left:7px">Contact</span></a></li>
+    </ul>
+  </div>
+  </nav>
+</div>
+
+<!-- /// Mobile Navigation Bar /// -->
+<ul class="side-nav" id="mobile-demo">
+  <li class="waves-effect waves-default"><a href="#"><img class="left" src="img/icons/gallery.svg"><span style="padding-left:7px">Gallery</span></a></li>
+  <li class="waves-effect waves-default"><a href="contact.html"><img class="left" src="img/icons/contact.svg"><span style="padding-left:7px">Contact</span></a></li>
+</ul>
