f7839f91066820ef10458e8f304f3502235fa9d8
atang90 <andrew.tang90@gmail.com>
2017-07-21 22:28:32 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<head>
+
+  <!-- /// Meta tags ///-->
+  <meta id='viewport' name="viewport" content="maximum-scale=1.0, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Throughthelens is a photo gallery">
+  <meta name="keywords" content="Photography, photo, gallery">
+
+  <!-- /// Import Materialize ///-->
+  <!--Import Google Icon Font=-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700|Ubuntu:300,400,700" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css" rel="stylesheet">
+
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection">
+  <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection">
+  <link type="text/css" rel="stylesheet" href="css/jquery.fancybox.min.css" media="screen,projection">
+
+</head>
