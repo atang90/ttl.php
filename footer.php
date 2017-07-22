1ec7eb168ddde652e1c2057d2628fdbf3dc30754
atang90 <andrew.tang90@gmail.com>
2017-07-21 22:28:05 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- /// Footer /// -->
+<footer class="page-footer light-blue darken-3">
+  <div class="footer-copyright" style="margin-left:10px"> Copyright &copy; <?php print date("Y"); ?> Andrew Tang</div>
+</footer>
+
+<!-- /// Import fancybox jQuery then import materialize.js /// -->
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<script type="text/javascript" src="css/jquery.fancybox.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
